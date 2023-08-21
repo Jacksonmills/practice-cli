cd96fdfa6778dc301b56d9cda47369a7f3d322bf
Jacksonmills <jacksonemills@gmail.com>
2023-08-21 11:31:00 -0500

    refactor package.json scripts and improve input validation in src/index.ts
    - Refactor "link-cli" script in package.json to handle cases where "unlink practice-cli" fails.
    - Improved input validation in "add" and "max" commands in src/index.ts to handle invalid numbers.

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,34 @@ program
   })
   .description('Logs a message to the console');
 
-program.command("add <numbers...>").action((numbers: number[]) => {
-  const total = numbers.reduce((a, b) => a + b, 0);
+program.command("add <numbers...>").action((numbers: string[]) => {
+  const total = numbers.reduce((a, b) => {
+    const numA = Number(a);
+    const numB = Number(b);
+
+    if (isNaN(numA) || isNaN(numB)) {
+      console.error("Invalid number");
+      process.exit(1);
+    };
+
+    return numA + numB;
+  }, 0);
   console.log(`Total: ${total}`);
 }).description("Adds a list of numbers");
 
-program.command("get-max <numbers...>").action((numbers: number[]) => {
-  const max = numbers.reduce((a, b) => Math.max(a, b));
+program.command("max <numbers...>").action((numbers: string[]) => {
+  const max = numbers.reduce((a, b) => {
+    const numA = Number(a);
+    const numB = Number(b);
+
+    if (isNaN(numA) || isNaN(numB)) {
+      console.error("Invalid number");
+      process.exit(1);
+    };
+
+    return numA > numB ? numA : numB;
+  }, 0);
+
   console.log(`Max: ${max}`);
 }).description("Gets the max number from a list of numbers");
 
