0618c468685f1eb230312db54ca505a2ff57754e
Jacksonmills <jacksonemills@gmail.com>
2023-08-21 10:57:38 -0500

    feat: Add initial project files and configurations
    This commit adds the following changes:
    - Added .gitignore file to exclude node_modules
    - Added index.js and index.ts files for CLI functionality
    - Updated package.json to include bin property and dev dependencies
    - Added pnpm-lock.yaml file for package locking
    - Added tsconfig.json for TypeScript configurations

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello = (m) => {
+    console.log(`Hello ${m}`);
+};
+hello("poo");
+hello("Jem");
+console.log("Hello World");
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const hello = (m: string) => {
+  console.log(`Hello ${m}`);
+};
+
+hello("poo");
+hello("Jem");
+console.log("Hello World");
