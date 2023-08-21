b69f741c009884fccf5d1434512143ff45b92c49
Jacksonmills <jacksonemills@gmail.com>
2023-08-21 11:16:36 -0500

    refactor(cli): Update script to link practice-cli package globally
    - The script "link-cli" in the package.json file was updated to include an additional command to make the "./dist/index.js" file executable.
    - This change ensures that the practice-cli package can be linked globally using the yarn --global link command.
    
    feat(cli): Add new commands for string and number manipulation
    
    - Added new commands to the CLI program:
      - "logs" command: Logs a given message to the console. It supports an optional "--caps" flag to log the message in all caps.
      - "add" command: Accepts a list of numbers and calculates their sum.
      - "get-max" command: Accepts a list of numbers and returns the maximum value.
    - These new commands enhance the functionality of the CLI program by providing more options for string and number manipulation.

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,29 @@
 import { Command } from 'commander';
 
 const program = new Command();
-program
-  .name('string-util')
-  .description('CLI to some JS string utils')
-  .version('0.8.0');
 
-program.action(() => {
-  console.log('Hello world!');
-}).description('Prints "Hello world!"');
+program
+  .argument("<string>", "String to log")
+  .option("-c, --caps", "Log in all caps")
+  .action((message: string, opts: {
+    caps?: boolean;
+  }) => {
+    if (opts.caps) {
+      console.log(message.toUpperCase());
+      return;
+    }
+    console.log(message);
+  })
+  .description('Logs a message to the console');
+
+program.command("add <numbers...>").action((numbers: number[]) => {
+  const total = numbers.reduce((a, b) => a + b, 0);
+  console.log(`Total: ${total}`);
+}).description("Adds a list of numbers");
+
+program.command("get-max <numbers...>").action((numbers: number[]) => {
+  const max = numbers.reduce((a, b) => Math.max(a, b));
+  console.log(`Max: ${max}`);
+}).description("Gets the max number from a list of numbers");
 
 program.parse(process.argv);
